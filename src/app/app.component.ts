75529dde7e3e5306ead16bdae682ee7f6045120f
Mauro Gonçalves <mauro@prodam.am.gov.br>
2019-04-12 10:32:11 -0400

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { UploadFileService } from './service/upload-file.service';
 import { Component } from '@angular/core';
+import { HttpClient, HttpResponse, HttpEventType } from '@angular/common/http';
+import {saveAs as importedSaveAs} from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'base-app';
+  selectedFiles: FileList;
+  currentFileUpload: File;
+  fileUrl;
+
+  constructor(
+    private uploadService: UploadFileService
+  ) {}
+
+  selectFile(event) {
+    this.selectedFiles = event.target.files;
+  }
+
+  upload() {
+    this.currentFileUpload = this.selectedFiles.item(0);
+    this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(event => {
+     if (event instanceof HttpResponse) {
+        console.log('File is completely uploaded!');
+      }
+    });
+    this.selectedFiles = undefined;
+  }
+
+  download() {
+    this.uploadService.report()
+    .subscribe(
+      data => {
+        importedSaveAs(data, 'bla.pdf');
+        /*const blob = new Blob([data], {type: 'application/pdf'});
+        const fileURL = URL.createObjectURL(blob);
+        /*this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+        //window.URL.createObjectURL(blob);
+        window.open(fileURL);*/
+      },
+      error => {
+        console.log(error);
+      }
+  );
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FileUploadModule } from 'ng2-file-upload';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
